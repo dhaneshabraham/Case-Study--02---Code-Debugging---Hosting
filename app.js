80c850c3aed80109b612eaa537796999a97395d2
dhaneshabraham <dhaneshabraham@gmail.com>
2022-05-06 18:11:15 +0530

    Case Study #02 - Code Debugging & Hosting

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express');     
+const app = new express();              //Part #1 Point 1
+const path = require ('path'); 
+const cors = require('cors');
+const bodyParser=require('body-parser');    // Part #1 Point 2 
+// const url='mongodb://localhost:27017/Library';
+// const mongoose = require('mongoose');
+// mongoose.connect(url,{useNewUrlParser:true});
+
+const nav= [
+    {
+        link:"/books",
+        title:"Books"
+    },
+    {
+        link:"/authors",
+        title:"Authors"
+    },
+    {
+        link:"/books/addbook",      //Part #2 Point 6
+        title:"Add Book"
+    },
+    {
+        link:"/authers/addauthor",  //Part #2 Point 6
+        title:"Add Author"
+    }
+]
+
+const loginRouter = require('./src/routes/loginroute');
+const signupRouter = require('./src/routes/signuproute');
+const homeRouter = require('./src/routes/homeroute');(nav);  //Part #1 Point 3,Part #2 Point 6
+const booksRouter = require('./src/routes/booksroute');(nav);//Part #2 Point 6
+const authorsRouter = require('./src/routes/authorsroute');(nav);//Part #2 Point 6
+
+
+
+app.set('views','./src/views'); 
+app.set('view engine','ejs'); 
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname , '/public'))); 
+app.use(cors()); //Part #2 Point 7
+
+app.use('/login',loginRouter); 
+app.use('/signup',signupRouter); 
+app.use('/home',homeRouter); 
+app.use('/books',booksRouter); 
+app.use('/authors',authorsRouter); 
+
+
+app.get('/',function(req,res){
+
+    res.render('index',{});
+    
+});
+
+
+
+const port =process.env.port || 3000;   //Part#1 Point 5
+app.listen(port,()=>{
+    console.log("Server Ready on 3000");
+});
+
